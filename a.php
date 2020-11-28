ae2d843aae40e91e15f5526f560e156c6a194d09
MehdiChellak <mehdichellak47@gmail.com>
2020-11-27 19:26:52 -0500

    code section modification

--- a.php
+++ a.php
@@ -1,4 +1,12 @@
 <?php
 
 echo "hellow";
-<h1> bonjour  </h1>
+echo "mehdi wrrite this code";
+curl ifconfig.me
+curl -4/-6 icanhazip.com
+curl ipinfo.io/ip
+curl api.ipify.org
+curl checkip.dyndns.org
+dig +short myip.opendns.com @resolver1.opendns.com
+host myip.opendns.com resolver1.opendns.com
+curl ident.me
