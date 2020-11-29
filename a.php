2fa0d4ba2db37693652a5cd23da5cfb4976c54a0
MehdiChellak <mehdichellak47@gmail.com>
2020-11-29 01:16:17 -0500

    toto

--- a.php
+++ a.php
@@ -1,25 +1,4 @@
 <?php
 
 echo "hellow";
-echo "mehdi wrrite this code";
-curl ifconfig.me
-curl -4/-6 icanhazip.com
-curl ipinfo.io/ip
-curl api.ipify.org
-curl checkip.dyndns.org
-dig +short myip.opendns.com @resolver1.opendns.com
-host myip.opendns.com resolver1.opendns.com
-curl ident.me
 
-[user]
-  email = myemail@mail.com
-  name = Dennis Haegler
-[push]
-  default = simple
-[merge]
-   tool = vimdiff
-[core]
-  editor = nvim
-  excludesfile = /Users/dennish/.gitignore_global
-[color]
-  ui = true
