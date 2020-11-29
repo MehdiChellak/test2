db0f8c6872b47b32bebc0cec107389261242b186
MehdiChellak <mehdichellak47@gmail.com>
2020-11-29 01:02:45 -0500

    stack over flow

--- a.php
+++ a.php
@@ -10,3 +10,16 @@ curl checkip.dyndns.org
 dig +short myip.opendns.com @resolver1.opendns.com
 host myip.opendns.com resolver1.opendns.com
 curl ident.me
+
+[user]
+  email = myemail@mail.com
+  name = Dennis Haegler
+[push]
+  default = simple
+[merge]
+   tool = vimdiff
+[core]
+  editor = nvim
+  excludesfile = /Users/dennish/.gitignore_global
+[color]
+  ui = true
